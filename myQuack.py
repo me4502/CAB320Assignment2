140fb8198c89dd10c59a25878c4b8aa4a13276a6
AstridJonelynas <astridjonelynas@gmail.com>
2017-05-13 09:53:45 +1000

    Completed my_team function and loaded dataset

--- myQuack.py
+++ myQuack.py
@@ -1,16 +1,14 @@
 
 '''
 
-Some partially defined functions for the Machine Learning assignment. 
+Some partially defined functions for the Machine Learning assignment.
 
 You should complete the provided functions and add more functions and classes as necessary.
- 
+
 Write a main function that calls different functions to perform the required tasks.
 
 '''
-
-
-
+import numpy as np
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -19,103 +17,108 @@ def my_team():
     '''
     Return the list of the team members of this assignment submission as a list
     of triplet of the form (student_number, first_name, last_name)
-    
+
     '''
-#    return [ (1234567, 'Ada', 'Lovelace'), (1234568, 'Grace', 'Hopper'), (1234569, 'Eva', 'Tardos') ]
-    raise NotImplementedError()
+    return [(8884731, 'Astrid', 'Jonelynas'),
+            (8847436, 'Lindsay', 'Watt'),
+            (9342401, 'Madeline', 'Miller')]
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 def prepare_dataset(dataset_path):
-    '''  
-    Read a comma separated text file where 
-	- the first field is a ID number 
-	- the second field is a class label 'B' or 'M'
-	- the remaining fields are real-valued
+    '''
+    Read a comma separated text file where
+        - the first field is a ID number
+        - the second field is a class label 'B' or 'M'
+        - the remaining fields are real-valued
 
-    Return two numpy arrays X and y where 
-	- X is two dimensional. X[i,:] is the ith example
-	- y is one dimensional. y[i] is the class label of X[i,:]
+    Return two numpy arrays X and y where
+        - X is two dimensional. X[i,:] is the ith example
+        - y is one dimensional. y[i] is the class label of X[i,:]
           y[i] should be set to 1 for 'M', and 0 for 'B'
 
     @param dataset_path: full path of the dataset text file
 
     @return
-	X,y
+        X,y
     '''
-    ##         "INSERT YOUR CODE HERE"    
-    raise NotImplementedError()
-
+    ##         "INSERT YOUR CODE HERE"
+    # not sure what X is supposed to have data wise?
+    # load data from file
+    data = np.genfromtxt(dataset_path, delimiter=',', dtype=None)
+    print(str(data))
+    return data
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 def build_NB_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Naive Bayes classifier based on the training set X_training, y_training.
 
-    @param 
-	X_training: X_training[i,:] is the ith example
-	y_training: y_training[i] is the class label of X_training[i,:]
+    @param
+        X_training: X_training[i,:] is the ith example
+        y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
-	clf : the classifier built in this function
+        clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 def build_DT_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Decision Tree classifier based on the training set X_training, y_training.
 
-    @param 
-	X_training: X_training[i,:] is the ith example
-	y_training: y_training[i] is the class label of X_training[i,:]
+    @param
+        X_training: X_training[i,:] is the ith example
+        y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
-	clf : the classifier built in this function
+        clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 def build_NN_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Nearrest Neighbours classifier based on the training set X_training, y_training.
 
-    @param 
-	X_training: X_training[i,:] is the ith example
-	y_training: y_training[i] is the class label of X_training[i,:]
+    @param
+        X_training: X_training[i,:] is the ith example
+        y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
-	clf : the classifier built in this function
+        clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+
 def build_SVM_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Support Vector Machine classifier based on the training set X_training, y_training.
 
-    @param 
-	X_training: X_training[i,:] is the ith example
-	y_training: y_training[i] is the class label of X_training[i,:]
+    @param
+        X_training: X_training[i,:] is the ith example
+        y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
-	clf : the classifier built in this function
+        clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if __name__ == "__main__":
-    pass
-    # call your functions here
-    
-
-
+#    print(my_team())
+    prepare_dataset('/home/astrid/Documents/CAB320/CAB320Assignment2/medical_records.data')
