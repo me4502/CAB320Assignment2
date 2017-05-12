8db222259ea6f158d903952f60e15784e959bf78
AstridJonelynas <astridjonelynas@gmail.com>
2017-05-13 08:51:48 +1000

    Initial Assignment Files

---
+++ myQuack.py
@@ -0,0 +1,121 @@
+
+'''
+
+Some partially defined functions for the Machine Learning assignment. 
+
+You should complete the provided functions and add more functions and classes as necessary.
+ 
+Write a main function that calls different functions to perform the required tasks.
+
+'''
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def my_team():
+    '''
+    Return the list of the team members of this assignment submission as a list
+    of triplet of the form (student_number, first_name, last_name)
+    
+    '''
+#    return [ (1234567, 'Ada', 'Lovelace'), (1234568, 'Grace', 'Hopper'), (1234569, 'Eva', 'Tardos') ]
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def prepare_dataset(dataset_path):
+    '''  
+    Read a comma separated text file where 
+	- the first field is a ID number 
+	- the second field is a class label 'B' or 'M'
+	- the remaining fields are real-valued
+
+    Return two numpy arrays X and y where 
+	- X is two dimensional. X[i,:] is the ith example
+	- y is one dimensional. y[i] is the class label of X[i,:]
+          y[i] should be set to 1 for 'M', and 0 for 'B'
+
+    @param dataset_path: full path of the dataset text file
+
+    @return
+	X,y
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_NB_classifier(X_training, y_training):
+    '''  
+    Build a Naive Bayes classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_DT_classifier(X_training, y_training):
+    '''  
+    Build a Decision Tree classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_NN_classifier(X_training, y_training):
+    '''  
+    Build a Nearrest Neighbours classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_SVM_classifier(X_training, y_training):
+    '''  
+    Build a Support Vector Machine classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if __name__ == "__main__":
+    pass
+    # call your functions here
+    
+
+
